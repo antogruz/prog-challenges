6b87d1c89f11b7624b237d85622127b99069a477
Antoine Gruzelle <antogruz@gmail.com>
2018-12-07 11:04:22 +0100

    Extract access_page method

--- request.py
+++ request.py
@@ -1,12 +1,16 @@
-sessid = "fc00efc3560fd888cb076344407a2e67"
-smfcookie89 = "a%3A4%3A%7Bi%3A0%3Bs%3A5%3A%2259438%22%3Bi%3A1%3Bs%3A40%3A%22ad298775288608b19f0382a471dd333380effb1f%22%3Bi%3A2%3Bi%3A1733388307%3Bi%3A3%3Bi%3A0%3B%7D"
-
-curl_request = "curl '{address}' -H 'Cookie: PHPSESSID={sessid}; SMFCookie89={smf}'"
 import os
 
+sessid = "fc00efc3560fd888cb076344407a2e67"
+smfcookie89 = "a%3A4%3A%7Bi%3A0%3Bs%3A5%3A%2259438%22%3Bi%3A1%3Bs%3A40%3A%22ad298775288608b19f0382a471dd333380effb1f%22%3Bi%3A2%3Bi%3A1733388307%3Bi%3A3%3Bi%3A0%3B%7D"
+curl_request = "curl '{address}' -H 'Cookie: PHPSESSID={sessid}; SMFCookie89={smf}' {params}"
+
 def get_page_with_cookie(address):
-    return os.popen(curl_request.format(address=address, sessid=sessid, smf=smfcookie89)).read()
+    return access_page(address)
 
 def get_file_with_cookie(address, output):
-    return os.popen((curl_request + " --output {output}").format(address=address, sessid=sessid, smf=smfcookie89, output=output)).read()
+    return access_page(address, "--output " + output)
+
+def access_page(address, params=""):
+    request = curl_request.format(address=address, sessid=sessid, smf=smfcookie89, params=params)
+    return os.popen(request).read()
 
